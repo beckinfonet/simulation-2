9eb1d5613d64ed36a5f927bb8ed2063dd03f5b50
Beck TJ (Bakytbek Tatibekov) <22246818+beckinfonet@users.noreply.github.com>
2021-01-28 22:42:36 -0800

    Delete index.js

--- web/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-
-import RootRouter from './router'
-
-ReactDOM.render(<RootRouter />, document.getElementById('root'));
-registerServiceWorker();
